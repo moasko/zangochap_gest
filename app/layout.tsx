005508317976af6aae5580673634893494c33212
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-07-06 01:40:27 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,6 +102,7 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+        <meta name="facebook-domain-verification" content="5124eegqyrok4swe5hcxi0wu445knt" />
       </head>
       {hasGoogleAnalytics && (
         <GoogleAnalytics gaId={googleAnalyticsId} />
